089b0dfb6d5c126edce9c5dacad1e76c620d8d6c
vwmaus <vwmaus1@gmail.com>
2015-04-01 19:33:44 +0200

    Code to compute the MODIS grid indexes from a pair of coordinates. And the code to compute the coordinates of a given MODIS grid position.

--- R/miscellaneous.R
+++ R/miscellaneous.R
@@ -1,4 +1,103 @@
 
+#' @title longitude and latitude to MODIS column and row. 
+#' 
+#' @description This function computes the MODIS grid 
+#' attributes from a pair of coordinates.
+#' 
+#' @param x An SpatialPoints object or the longitude.
+#' @param pixelsize A number. The MODIS resolution.
+#' @param projCRS A CRS object.
+#' 
+#' @docType methods
+#' @export
+longLatTomodisColRow = function(x, y=NULL, pixelsize, projCRS=NULL){
+  require(sp)
+  projCRSSinu = CRS("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
+  tilesize = 1111950.51966666709631681442
+  nhtiles = 36
+  nvtiles = 18
+  ncells = trunc(tilesize/pixelsize)
+  xmin = - tilesize*nhtiles/2
+  ymax = tilesize*nvtiles/2
+  
+  if( class(x)!="SpatialPoints" & is.null(projCRS))
+    stop("Missin either an SpatialPoints object or the CRS projection")
+  
+  sppoint = x
+  if( class(sppoint)!="SpatialPoints" )
+    sppoint = SpatialPoints(coords = data.frame(longitude=x, latitude=y), projCRS)
+  
+  longitude = coordinates(sppoint)[,"longitude"]
+  latitude = coordinates(sppoint)[,"latitude"]
+  sppoint = spTransform(sppoint, projCRSSinu)
+  x = coordinates(sppoint)[,"longitude"]
+  y = coordinates(sppoint)[,"latitude"]
+  
+  dx = x - xmin
+  dy = ymax - y 
+  
+  col = trunc(dx/pixelsize)
+  row = trunc(dy/pixelsize)
+  
+  h = abs(trunc(dx / tilesize))
+  v = abs(trunc(dy / tilesize))
+  
+  j = col - ncells * h
+  i = row - ncells * v
+  
+  res = data.frame(longitude, latitude, col, row, h, v, j, i)
+  rownames(res) = NULL
+  res
+}
+
+
+
+
+#' @title MODIS column and row to longitude and latitude. 
+#' 
+#' @description This function computes the longitude and latitude for a
+#' MODIS column and row index.
+#' 
+#' @param col An integer. A global index of the MODIS column
+#' or a local index of the column within a specific h:v tile
+#' @param row An integer. A global index of the MODIS row
+#' or a local index of the row within a specific h:v tile
+#' @param h An integer. Horizontal MODIS tile 
+#' @param v An integer. Vertical MODIS tile 
+#' @param pixelsize A number. The MODIS resolution.
+#' @param projCRS A CRS object.
+#' 
+#' @docType methods
+#' @export
+modisColRowToLongLat = function(col, row, h=NULL, v=NULL, pixelsize, projCRS=NULL){
+  require(sp)
+  projCRSSinu = CRS("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
+  if(class(projCRS)!="CRS")
+    projCRS = CRS("+proj=longlat +datum=WGS84 +no_defs")
+  
+  tilesize = 1111950.51966666709631681442
+  ncells = trunc(tilesize/pixelsize)
+  nhtiles = 36
+  nvtiles = 18
+  
+  nhcells = ncells*nhtiles/2
+  nvcells = ncells*nvtiles/2
+  j = col - nhcells
+  i = nvcells - row
+  if( !is.null(h) )
+    j = ncells*(h - nhtiles/2) + col
+  if( !is.null(v) )
+    i = ncells*(nvtiles/2 - v) - row 
+  
+  longitude = pixelsize*j + pixelsize/2
+  latitude = pixelsize*i - pixelsize/2
+  sppoint = SpatialPoints(coords = data.frame(longitude, latitude), projCRSSinu)
+  sppoint = spTransform(sppoint, projCRS)
+  res = data.frame(coordinates(sppoint))
+  res
+}
+
+
 #' @title Splits the total number of processes
 #' 
 #' @description A recursive function to splits the total 
@@ -141,11 +240,11 @@ buildSciDBDTWQuery = function(INPUTARRAY, OUTPUTSCHEMA, PATTERNNAMES,
                               TMIN, TMAX,
                               COLIDS, ROWIDS,
                               JUNKCOL, JUNKROW,
-			      THRESHOLD, NORMALIZE,
-			      REALDAY, METHOD,
-			      THETA, ALPHA,
-			      BETA, DELAY,
-			      NCORES,
+                  			      THRESHOLD, NORMALIZE,
+                  			      REALDAY, METHOD,
+                  			      THETA, ALPHA,
+                  			      BETA, DELAY,
+                  			      NCORES,
                               LIBRARYPATH, EXPPRPATH){
 
 rOut = 0:(length(PATTERNNAMES)+2)
@@ -174,15 +273,15 @@ res = paste(" redimension(
                                                           LIBRARYPATH=\"",LIBRARYPATH,"\"
                                                           FROM=\"",FROM,"\"
                                                           TO=\"",TO,"\"
-							  THRESHOLD=",THRESHOLD,"
-							  NORMALIZE=",NORMALIZE,"
-							  REALDAY=",REALDAY,"
-							  METHOD=\"",METHOD,"\"
-							  THETA=",THETA,"
-							  ALPHA=",ALPHA,"
-							  BETA=",BETA,"
-							  DELAY=",DELAY,"
-							  NCORES=",NCORES,"
+                                          							  THRESHOLD=",THRESHOLD,"
+                                          							  NORMALIZE=",NORMALIZE,"
+                                          							  REALDAY=",REALDAY,"
+                                          							  METHOD=\"",METHOD,"\"
+                                          							  THETA=",THETA,"
+                                          							  ALPHA=",ALPHA,"
+                                          							  BETA=",BETA,"
+                                          							  DELAY=",DELAY,"
+                                          							  NCORES=",NCORES,"
                                                           source(\"",EXPPRPATH,"\")
                                                           res'
                                              ),
@@ -196,43 +295,9 @@ res = paste(" redimension(
                         ),",
                         OUTPUTSCHEMA,"
               )", sep="")
-
-# res = paste(" redimension(
-#                         project(
-#             apply(
-#             attribute_rename(
-#             r_exec(
-#             project(
-#             apply(TMP_1, devi, double(evi), dcol, double(col_id), drow, double(row_id), dtime, double(time_id)
-#             ), 
-#             devi, dcol, drow, dtime
-#             ),
-#             'output_attrs=",length(rOut),"',
-#             'expr=
-#             LIBRARYPATH=\"",LIBRARYPATH,"\"
-#             FROM=\"",FROM,"\"
-#             TO=\"",TO,"\"
-#             THETA=",THETA,"
-#             SPAN=",SPAN,"
-#             OVERLAPPING=",OVERLAPPING,"
-#             THRESHOLD=",THRESHOLD,"
-#             NORMALIZE=",NORMALIZE,"
-#             SATSTAT=",SATSTAT,"
-#             NCORES=",NCORES,"
-#             source(\"",EXPPRPATH,"\")
-#                                                           res'
-#                                              ),
-#                                              ",attrRename,"
-#                                      ),
-#                                      col_id,  int64(col),
-#                                      row_id,  int64(row),
-#                                      year_id, int64(year)
-#                               ),",
-#             attrRedimension,"
-#                         ),",
-#             OUTPUTSCHEMA,"
-#               )", sep="")
 return(res)
 
 }
+
+
 

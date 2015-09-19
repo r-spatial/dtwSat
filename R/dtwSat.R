c7ca533d08b8f0cbcddb7ecf0b66fff1e4e1c4c3
vwmaus <vwmaus1@gmail.com>
2015-09-19 18:29:23 +0200

    Include methods for twdtw class.

--- R/dtwSat.R
+++ R/dtwSat.R
@@ -1,5 +1,5 @@
 
-#' @title Satellite image time series smoothing. 
+#' @title Satellite image time series smoothing
 #' 
 #' @description This function applies a smoothing algorithm to 
 #' the satellite image time series. It allows to compute the 
@@ -125,6 +125,7 @@ twdtw =  function(query, template, weight=NULL, dist.method="Euclidean",
   wm = matrix(FALSE, nrow = n, ncol = m)
   wm[window.function(row(wm), col(wm), query.size = n, reference.size = m)] = TRUE
   out = .Call("computeCM_Call", PACKAGE="dtw", wm, delta, cm, step.matrix)
+  out$call = match.call()
   out$stepPattern = step.matrix
   out$costMatrix = out$costMatrix[-1,]
   out$directionMatrix = out$directionMatrix[-1,]
@@ -151,7 +152,7 @@ twdtw =  function(query, template, weight=NULL, dist.method="Euclidean",
       endPoints = endPoints[1:alignments]
     # Map low cost paths (k-th paths)
     mapping = lapply(endPoints, function(b){
-      return(kthbacktrack(out, b))
+      return(.kthbacktrack(out, b))
     })
     
     # Get the starting point of each path
@@ -167,8 +168,7 @@ twdtw =  function(query, template, weight=NULL, dist.method="Euclidean",
                                 stringsAsFactors = FALSE)
     out$mapping = mapping
   }
-  out$call = match.call()
-#   out$stepPattern = 
+  
   class(out) = "twdtw"
   return(out)
 }
@@ -196,302 +196,46 @@ twdtw =  function(query, template, weight=NULL, dist.method="Euclidean",
 #' @description Methods for plotting Time-Weighted DTW objects 
 #' returned by twdtw.
 #' 
-#' @param x twdtw object
-#' @param n An integer. The alignment. Default is 1
-#' @param d An integer. The dimension for point-by-point 
-#' comparison, with matching lines \code{\link{dtwPlotThreeWay}} 
-#' in package \pkg{dtw}. Default is 1
-#' @param type see \code{\link{plot.dtw}} in package \pkg{dtw}. 
-#' Default is "density"
-#' @param ... see \code{plot.dtw} in package \pkg{dtw}
+#' @param obj twdtw object
+#' @param ... additional arguments passed to plotting functions
 #' @docType methods
 #' @export
-plotTwdtw = function(x, n=1, d=1, type="density", ...){
-  if(0 > n & n > length(x$mapping))
-    stop(paste("n must be an integer between 1 and the number of alignments:",length(x$mapping)))
-  if(0 > d & d > ncol(x$query) )
-    stop(paste("d must be an integer between 1 and the number of cols in the query:",ncol(x$query)))
-  obj = .dtwObj(x, n=1, d=1)
-  dtw::dtwPlot(obj, type="density", ...)
+plot.twdtw = function(x, ylab="Pattern", xlab="Time series", ...){
+  tx = index(x$template)
+  ty = index(x$query)
+  m = t(x$costMatrix)
+  image(m, col=terrain.colors(100), x=tx, y=ty, xlab=xlab, ylab=ylab, ...)
+  contour(m, x=tx, y=ty, add=TRUE)
+  for(i in 1:length(x$mapping))
+    lines(tx[x$mapping[[i]]$index2], ty[x$mapping[[i]]$index1] ,col="red", lwd=2)
 }
 
-# PLOT ALL ALIGNMENTS IN THE SAME TERREAIN
-# RUN LIST OF PATTERNS AND RETRIENVE A DATA.FRAME
-
-.dtwObj = function(x, n=1, d=1){
-  if(class(x)!="twdtw")
-    stop("Missing a twdtw object.")
-  res = x
-  res$jmin      = x$alignments$to
-  res$index1    = x$mapping[[n]]$index1
-  res$index2    = x$mapping[[n]]$index2
-  res$query     = x$query[,d]
-  res$reference = x$template[,d]
-  return(res)
+#' @export
+is.twdtw = function(x){
+  return(inherits(x,"twdtw"))
 }
 
-#' @title Function to compute phenological metrics. 
+#' @export
+print.twdtw = function(x,...){
+  head = "Time-Weighted DTW alignment object\n"
+  size = sprintf("Alignment size (query x template): %d x %d\n", x$N, x$M)
+  alignments = sprintf("Number of alignment: %d\n", nrow(x$alignments))
+  call = sprintf("Call: %s\n", deparse(x$call))
+  cat(head,size,alignments,call)
+}
+
+
+#' @title DTW backtrack
 #' 
-#' @description This compute phenological metrics from a time series.
+#' @description This function preforms the backtrack starting from  
+#' a given index of the last line in the global cost matrix. 
 #' 
-#' @param y A zoo object with the query time series.
-#' @param a A vector. Indices in y of alignment starts.
-#' @param b A vector. Indices in y of alignment ends.
+#' @param alignment A twdtw or dtw alignment object. 
+#' @param jmin An integer. The index of the last line in the 
+#' global cost matrix. 
 #' @docType methods
 #' @export
-statTimeSat = function(y, a=NULL, b=NULL){
-  if(!is.zoo(y))
-    stop("Missing zoo object. The query must be a zoo object.")
-  ty = as.Date(index(y))
-  y  = as.numeric(y)
-  if(is.null(a)) a = 1
-  if(is.null(b)) b = length(y)
-  out = do.call("rbind", lapply(seq_along(a), function(k){
-    imax = which.max(y[a[k]:b[k]]) + a[k] - 1
-    dy = diff(y[a[k]:b[k]])
-    ipics = which(dy[-1] < 0 & 0 < dy[-length(dy)]) + a[k] + 1
-    if( length(ipics)==0 )
-      ipics = imax
-    iminL = which.min(y[a[k]:ipics[1]]) + a[k] - 1
-    iminR = which.min(y[ipics[length(ipics)]:b[k]]) + ipics[length(ipics)] - 1
-    dateLeftMin = ty[iminL]
-    dateRightMin = ty[iminR]
-    dateMax = ty[imax]
-    pics = length(ipics)
-    slength = as.numeric(ty[b[k]] - ty[a[k]])
-    leftMin = y[iminL]
-    rightMin = y[iminR]
-    smax = y[imax]
-    smin = mean(leftMin, rightMin, na.rm=TRUE)
-    smplitude = smax - smin
-    smean = mean(y[a[k]:b[k]], na.rm=TRUE)
-    ssd = sd(y[a[k]:b[k]], na.rm=TRUE)
-    leftSlop = (y[ipics[1]] - leftMin) / abs(as.numeric(ty[ipics[1]] - ty[iminL]))
-    rightSlop = (rightMin - y[ipics[length(ipics)]]) / abs(as.numeric(ty[ipics[length(ipics)]] - ty[iminL]))
-    totInt = sum(y[a[k]:b[k]], na.rm=TRUE)
-    seasonInt = sum(y[a[k]:b[k]]-smin, na.rm=TRUE)
-    return(
-      data.frame(
-        dateLeftMin = dateLeftMin,
-        dateRightMin = dateRightMin,
-        dateMax = dateMax,
-        pics = pics,
-        length = slength,
-        leftMin = leftMin,
-        rightMin = rightMin,
-        max = smax,
-        min = smin,
-        mean = smean,
-        sd = ssd,
-        leftSlop = leftSlop,
-        rightSlop = rightSlop,
-        totInt = totInt,
-        seasonInt = seasonInt
-      ))
-  }))
-  
-  return(out)
-}
-
-
-#' @title Satellite image time series classification. 
-#' 
-#' @description This function classify the time segments based on the 
-#' open boundary DTW analysis results (i.e. results of timeSeriesAnalysis 
-#' function).
-#' 
-#' @param dtwResults A list of dtw . 
-#' @param from A character with the start date for the finalclassification.
-#' The format is yyyy-mm-dd.
-#' @param to A character with the end date for the finalclassification. The 
-#' format is yyyy-mm-dd.
-#' @param by An integer. The length in months of the time subsequences
-#' for the final classification. Default is 12 months.
-#' @param overlapping A real between 0 and 1 representing the percentage of
-#' minimum overlapping between the DTW mach and the subsequences for the 
-#' final classification. Default is 40\%.
-#' @param threshold A real with the DTW threshold, i.e. the maximum DTW cost 
-#' for consideration.
-#' @param sortBydtw A logical. If TRUE the function returns the best 
-#' classification for each period of time. If FALSE the functions 
-#' returns the lowest dtw for each class. Default is TRUE.
-#' @param aggregateByClass A logical. Aggregate results by common class. 
-#' Default is TRUE
-#' @docType methods
-#' @export
-timeSeriesClassifier = function(dtwResults, from, to, by=12,
-                                overlapping=0.4, threshold=4.0, 
-                                sortBydtw=TRUE, aggregateByClass=TRUE)
-{
-    if(!is.list(dtwResults))
-      stop("Missing a list. The parameter dtwResults must be a list.")
-    
-    if( !(is.character(from) | class(from)=="Date") )
-      stop("Missing a character. The parameter from must be a character or a Date in the formt yyyy-mm-dd.")
-    
-    if(!(is.character(to) | class(to)=="Date") )
-      stop("Missing a character. The parameter to must be a character or a Date in the formt yyyy-mm-dd.")
-    
-    if( overlapping < 0 & 1 < overlapping )
-      stop("Error: overlapping must be a number between 0 and 1.")
-    
-    from = as.Date(from, origin="1970-01-01")
-    to = as.Date(to, origin="1970-01-01")
-    startDates = seq(from, to, by = paste(by, "month"))
-    endDates = seq(startDates[2], to+by-1, by = paste(by, "month"))
-    years = as.numeric(format(endDates,"%Y"))
-    
-    className = names(dtwResults)
-    if(is.null(className))
-      className = paste("class",seq_along(dtwResults),sep="_")
-    
-    # Find the lowest cost for each class
-    aux = lapply( dtwResults, function(x){
-       out = do.call("rbind", lapply(seq_along(years), function(k){
-         .lowestDTWCostInOverlapping(x, years[k], startDates[k], endDates[k], overlapping)
-       }))
-    })
-    aux = data.frame(aux)
-    
-    if(sortBydtw){
-      # Find the best classification based on DTW cost
-      res = do.call("rbind", lapply(seq_along(years), function(k){
-        subsequence = aux[k,grep(names(aux), pattern="distance")]
-        out = .bestDTWClass(subsequence, threshold=threshold)
-        data.frame(year=years[k], out)
-      }))
-      return(res)
-    }
-
-    
-    if(aggregateByClass){
-      res = lapply(c("years", unique(className)), function(name){
-        if(name=="years")
-          return(years)
-        out = unlist(lapply(seq_along(years), function(k){
-          subsequence = aux[k,grep(names(aux), pattern=name)]
-          dtwcost = subsequence[grep(names(subsequence), pattern="distance")]
-          dtwcost[is.na(dtwcost)] = threshold 
-          return(min(as.numeric(dtwcost), na.rm=TRUE))
-        }))
-        return(out)
-      })
-      names(res) = c("year", unique(className))
-      return(res)
-    }
-    
-    names(dtwResults) = paste(names(dtwResults),seq_along(dtwResults),sep="_")
-    
-    res = lapply(dtwResults, function(x){
-      unlist(lapply(years, function(y){
-          I = which(as.numeric(format(x$to, "%Y"))==y)
-          if(length(I)==0)
-            return(NA)
-          return(min(x$distance[I]))
-      }))
-    })
-    
-    return(data.frame(year=years, res))  
-
-}
-
-
-.bestDTWClass = function(x, threshold)
-{
-  x[x > threshold] = NA
-  x = x[order(x)]
-  
-  classNames = data.frame(lapply(names(x), function(name){
-    unlist(strsplit(name, split="\\."))[1]
-  }), stringsAsFactors = FALSE )
-  
-  classNames[is.na(x)] = "other"
-  
-  names(classNames) = paste("class", seq_along(x), sep=".")
-  names(x) = paste("distance", seq_along(x), sep=".")
-  
-  return(data.frame(classNames, x))
-}
-
-
-.lowestDTWCostInOverlapping = function(x, year, startDate, endDate, overlapping)
-{
-    I = which( year == as.numeric(format(x$to,"%Y")) )
-    
-    # Test minimum overlapping 
-    flag = unlist(lapply(I, function(i){
-      a = x$from[i]
-      if(a < startDate)
-        a = startDate
-      b = x$to[i]
-      if(endDate < b)
-        b = endDate
-      
-      if(a >= endDate | b <= startDate)
-        return(FALSE)
-      
-      subsequencesOverlapping = as.numeric(b - a) / as.numeric(endDate - startDate)
-      return(subsequencesOverlapping >= overlapping)
-    }))
-    
-    if(!any(flag))
-      return(data.frame(year = year, 
-                        distance = NA,
-                        stringsAsFactors = FALSE))
-    
-    # Return the lowest DTW cost
-    I = I[which.min(x$distance[I[flag]])]
-    return(data.frame(year = year, 
-                      distance = x$distance[I], 
-                      stringsAsFactors = FALSE))
-
-}
-
-
-#' @title Computes the accuracy of a classification
-#' 
-#' @description This function computs the accuracy a classification based
-#' on the a given dataset of controll points.
-#' 
-#' @param predicted A vector of prediction. 
-#' @param reference A vector of reference for the predicted classes. 
-#' @docType methods
-#' @export
-computeAccuracy = function(predicted, reference)
-{
-
-  if( length(predicted)!=length(reference) )
-    stop("The vectors 'predicted and 'reference' must have the same length.")
-  
-  factorLevels = unique( c(reference,predicted) )
-  
-  # Global accuracy
-  global_accuracy = sum(predicted==reference) / length(reference)
-  
-  a = factor(predicted, levels = factorLevels)
-  b = factor(reference, levels = factorLevels)
-    
-  # Check predicted classes against reference class labels
-  confusion_table = table(predicted=a, reference=b)
-  
-  return(list(global_accuracy = global_accuracy, 
-              confusion_table = confusion_table))
-}
-
-
-
-#' @title Computes the backtrack of dtw
-#' 
-#' @description This function computs the backtrack starting from  
-#' a given index of the last line of the global cost matrix. 
-#' 
-#' @param alignment A dtw alignment object. 
-#' @param jmin An integer. index of the last line of the global 
-#' cost matrix. 
-#' @docType methods
-#' @export
-
-kthbacktrack = function(alignment, jmin=NULL) {
+.kthbacktrack = function(alignment, jmin=NULL) {
   
   dir = alignment$stepPattern
   npat = attr(dir,"npat")
--- R/miscellaneous.R
+++ R/miscellaneous.R
@@ -1,24 +1,207 @@
-#' @title Computes DTW distance
+#' @title Performs multiple Time-Weighted DTW 
 #' 
-#' @description The function computes the whole possible alignments 
-#' for a set of patterns in one time series. 
+#' @description The function performs the Time-Weighted DTW for a list 
+#' of queries
 #' 
+#' @param query A list of zoo objects with the multidimensional 
+#' time series
 #' @param template A zoo object with the template time series. 
-#' It must be larger than the query.
-#' @param TemporalPatterns.list A list of patterns. Each node of the list 
-#' has two columns data.frame with the dates and the value. 
+#' It must be iguel or be equal or longer than the length of the query and 
+#' the same number of dimensions
+#' @param ... See \code{\link[proxy]{twdtw}}
 #' @docType methods
 #' @export
-computeDTWForAllPatterns = function(template, TemporalPatterns.list, ... ){
-  res = lapply(seq_along(TemporalPatterns.list), function(j){
-    patternName = names(TemporalPatterns.list)[j]
-    x = as.numeric(TemporalPatterns.list[[j]][,2])
-    tx = as.Date(TemporalPatterns.list[[j]][,1], origin="1970-01-01")
-    query = zoo(x, tx)
-    out = timeSeriesAnalysis(query, template, theta=THETA, span=1,
-                             normalize=TRUE)
-    return(out)
-  })
-  names(res) = names(TemporalPatterns.list)
+mtwdtw = function(query, template, ...){
+  if(!is.list(query))
+    stop("Missing a list of zoo objects. The query must be a list zoo objects.")
+  res = do.call("rbind", lapply(query, function(i){
+    data.frame(query=i, twdtw(query[[i]], template, ...)$alignments)
+  }))
   return(res)
+}
+
+
+
+#' @title Classification accuracy  
+#' 
+#' @description This function retrieves the accuracy of classification
+#' based on the a given dataset of controll points.
+#' 
+#' @param predicted A vector of prediction. 
+#' @param reference A vector of reference for the predicted classes. 
+#' @docType methods
+#' @export
+computeAccuracy = function(predicted, reference)
+{
+  
+  if( length(predicted)!=length(reference) )
+    stop("The vectors 'predicted and 'reference' must have the same length.")
+  
+  factorLevels = unique( c(reference,predicted) )
+  
+  # Global accuracy
+  global_accuracy = sum(predicted==reference) / length(reference)
+  
+  a = factor(predicted, levels = factorLevels)
+  b = factor(reference, levels = factorLevels)
+  
+  # Check predicted classes against reference class labels
+  confusion_table = table(predicted=a, reference=b)
+  
+  return(list(global_accuracy = global_accuracy, 
+              confusion_table = confusion_table))
+}
+
+
+#' @title Satellite image time series classification. 
+#' 
+#' @description This function classify the time segments based on the 
+#' open boundary DTW analysis results (i.e. results of timeSeriesAnalysis 
+#' function).
+#' 
+#' @param dtwResults A list of dtw . 
+#' @param from A character with the start date for the finalclassification.
+#' The format is yyyy-mm-dd.
+#' @param to A character with the end date for the finalclassification. The 
+#' format is yyyy-mm-dd.
+#' @param by An integer. The length in months of the time subsequences
+#' for the final classification. Default is 12 months.
+#' @param overlapping A real between 0 and 1 representing the percentage of
+#' minimum overlapping between the DTW mach and the subsequences for the 
+#' final classification. Default is 40\%.
+#' @param threshold A real with the DTW threshold, i.e. the maximum DTW cost 
+#' for consideration.
+#' @param sortBydtw A logical. If TRUE the function returns the best 
+#' classification for each period of time. If FALSE the functions 
+#' returns the lowest dtw for each class. Default is TRUE.
+#' @param aggregateByClass A logical. Aggregate results by common class. 
+#' Default is TRUE
+#' @docType methods
+#' @export
+timeSeriesClassifier = function(dtwResults, from, to, by=12,
+                                overlapping=0.4, threshold=4.0, 
+                                sortBydtw=TRUE, aggregateByClass=TRUE)
+{
+  if(!is.list(dtwResults))
+    stop("Missing a list. The parameter dtwResults must be a list.")
+  
+  if( !(is.character(from) | class(from)=="Date") )
+    stop("Missing a character. The parameter from must be a character or a Date in the formt yyyy-mm-dd.")
+  
+  if(!(is.character(to) | class(to)=="Date") )
+    stop("Missing a character. The parameter to must be a character or a Date in the formt yyyy-mm-dd.")
+  
+  if( overlapping < 0 & 1 < overlapping )
+    stop("Error: overlapping must be a number between 0 and 1.")
+  
+  from = as.Date(from, origin="1970-01-01")
+  to = as.Date(to, origin="1970-01-01")
+  startDates = seq(from, to, by = paste(by, "month"))
+  endDates = seq(startDates[2], to+by-1, by = paste(by, "month"))
+  years = as.numeric(format(endDates,"%Y"))
+  
+  className = names(dtwResults)
+  if(is.null(className))
+    className = paste("class",seq_along(dtwResults),sep="_")
+  
+  # Find the lowest cost for each class
+  aux = lapply( dtwResults, function(x){
+    out = do.call("rbind", lapply(seq_along(years), function(k){
+      .lowestDTWCostInOverlapping(x, years[k], startDates[k], endDates[k], overlapping)
+    }))
+  })
+  aux = data.frame(aux)
+  
+  if(sortBydtw){
+    # Find the best classification based on DTW cost
+    res = do.call("rbind", lapply(seq_along(years), function(k){
+      subsequence = aux[k,grep(names(aux), pattern="distance")]
+      out = .bestDTWClass(subsequence, threshold=threshold)
+      data.frame(year=years[k], out)
+    }))
+    return(res)
+  }
+  
+  
+  if(aggregateByClass){
+    res = lapply(c("years", unique(className)), function(name){
+      if(name=="years")
+        return(years)
+      out = unlist(lapply(seq_along(years), function(k){
+        subsequence = aux[k,grep(names(aux), pattern=name)]
+        dtwcost = subsequence[grep(names(subsequence), pattern="distance")]
+        dtwcost[is.na(dtwcost)] = threshold 
+        return(min(as.numeric(dtwcost), na.rm=TRUE))
+      }))
+      return(out)
+    })
+    names(res) = c("year", unique(className))
+    return(res)
+  }
+  
+  names(dtwResults) = paste(names(dtwResults),seq_along(dtwResults),sep="_")
+  
+  res = lapply(dtwResults, function(x){
+    unlist(lapply(years, function(y){
+      I = which(as.numeric(format(x$to, "%Y"))==y)
+      if(length(I)==0)
+        return(NA)
+      return(min(x$distance[I]))
+    }))
+  })
+  
+  return(data.frame(year=years, res))  
+  
+}
+
+
+.bestDTWClass = function(x, threshold)
+{
+  x[x > threshold] = NA
+  x = x[order(x)]
+  
+  classNames = data.frame(lapply(names(x), function(name){
+    unlist(strsplit(name, split="\\."))[1]
+  }), stringsAsFactors = FALSE )
+  
+  classNames[is.na(x)] = "other"
+  
+  names(classNames) = paste("class", seq_along(x), sep=".")
+  names(x) = paste("distance", seq_along(x), sep=".")
+  
+  return(data.frame(classNames, x))
+}
+
+
+.lowestDTWCostInOverlapping = function(x, year, startDate, endDate, overlapping)
+{
+  I = which( year == as.numeric(format(x$to,"%Y")) )
+  
+  # Test minimum overlapping 
+  flag = unlist(lapply(I, function(i){
+    a = x$from[i]
+    if(a < startDate)
+      a = startDate
+    b = x$to[i]
+    if(endDate < b)
+      b = endDate
+    
+    if(a >= endDate | b <= startDate)
+      return(FALSE)
+    
+    subsequencesOverlapping = as.numeric(b - a) / as.numeric(endDate - startDate)
+    return(subsequencesOverlapping >= overlapping)
+  }))
+  
+  if(!any(flag))
+    return(data.frame(year = year, 
+                      distance = NA,
+                      stringsAsFactors = FALSE))
+  
+  # Return the lowest DTW cost
+  I = I[which.min(x$distance[I[flag]])]
+  return(data.frame(year = year, 
+                    distance = x$distance[I], 
+                    stringsAsFactors = FALSE))
+  
 }

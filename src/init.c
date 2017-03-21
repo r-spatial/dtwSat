b696b196c3b04a9461a5d037bf064f5cf48c1888
vwmaus <vwmaus1@gmail.com>
2017-03-21 15:02:46 +0100

    Fix warning no call to: 'R_registerRoutines'.

---
+++ src/init.c
@@ -0,0 +1,27 @@
+#include <R_ext/RS.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+ Check these declarations against the C/Fortran source code.
+ */
+
+/* .Fortran calls */
+extern void F77_NAME(bestmatches)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(computecost)(void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(g)(void *, void *, void *, void *);
+extern void F77_NAME(tracepath)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+  {"bestmatches", (DL_FUNC) &F77_NAME(bestmatches), 11},
+  {"computecost", (DL_FUNC) &F77_NAME(computecost),  7},
+  {"g",           (DL_FUNC) &F77_NAME(g),            4},
+  {"tracepath",   (DL_FUNC) &F77_NAME(tracepath),   11},
+  {NULL, NULL, 0}
+};
+
+void R_init_dtwSat(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}

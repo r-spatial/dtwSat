407121d005590340e9dcc594fce351dce80e5e69
vwmaus <vwmaus1@gmail.com>
2023-07-25 22:37:14 +0200

    Fixes labels in ts extract

--- tests/testthat/test-twdtw_classify.R
+++ tests/testthat/test-twdtw_classify.R
@@ -11,15 +11,15 @@ tif_files <- dir("inst/mato_grosso_brazil", pattern = "\\.tif$", full.names = TR
 # MOD13Q1 is a 16-day composite product, so the acquisition date is the first day of the 16-day period
 acquisition_date <- as.Date(str_extract(tif_files, "[0-9]{8}"), format = "%Y%m%d")
 
-# Read the data as a stars object and set band names
+# Read the data as a stars object, setting time as a dimension and band as attribute
 dc <- read_stars(tif_files, proxy = FALSE, along = list(time = acquisition_date)) |>
   st_set_dimensions(3, c("EVI", "NDVI", "RED", "BLUE", "NIR", "MIR", "DOY")) |>
   split("band")
 
-# Remove the DOY band - this will be supported int he future
+# Remove the DOY band - this will be supported int the future
 dc <- dc[c("EVI", "NDVI", "RED", "BLUE", "NIR", "MIR")]
 
-# extract the samples
+# Extract the samples
 ts_samples <- st_extract(dc, samples)
 ts_samples$id <- 1:dim(ts_samples)["geom"]
 ts_samples$label <- samples$label
@@ -29,6 +29,6 @@ a$geom <- NULL
 
 head(a)
 
-a[a$id == "1",]
+a[a$id == "1", ]
 
 # training set ready!
